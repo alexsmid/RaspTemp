6a13a1775d59b13ef2fa1df79e5fb419e9797c34
alexsmid <alex.smiding@gmail.com>
2021-11-18 19:17:03 +0100

    fix

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-   templateData = {
-      'temperature' : 23,
-      'updated' : 2020-01-10
-   }
-   return render_template('main.html', **templateData)
+    templateData = {
+        'temperature' : 23,
+        'updated' : 2020-01-10
+    }
+    return render_template('main.html', **templateData)
 
 if __name__ == "__main__":
     from waitress import serve
-    serve(host='0.0.0.0', port=80, debug=True)
+    serve(app,host='0.0.0.0', port=80)
